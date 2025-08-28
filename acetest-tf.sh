a75176bb85c8e58ef3eab30f92675b36016f152e
canarypwn <canarypwn@aosc.io>
2025-08-28 17:03:21 -0400

    Add SLURM support to acetest and pathfinder scripts for TensorFlow and Torch

--- acetest-tf.sh
+++ acetest-tf.sh
@@ -5,4 +5,4 @@
 
 # python3 -u run.py --dll tf --version 2.16 --mode cov --itv 60 --time_budget 1200 --num_parallel 55 --vs pathfinder
 # python3 -u run.py --dll torch --version 2.2 --mode cov --itv 60 --time_budget 1200 --num_parallel 10 
-python3 -u run.py --dll tf --version 2.16 --mode cov --itv 60 --time_budget 600 --num_parallel 55 --vs acetest
+python3 -u run.py --dll tf --version 2.16 --mode cov --itv 60 --time_budget 600 --num_parallel 50 --vs acetest --slurm
---
+++ pathfinder-torch.sh
@@ -0,0 +1,1 @@
+python3 -u run.py --dll torch --version 2.2 --mode cov --itv 60 --time_budget 600 --num_parallel 50 --vs pathfinder --slurm
