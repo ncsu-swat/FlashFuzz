c139264182cedac03e462e29eb54fd8be35888b0
canarypwn <canarypwn@aosc.io>
2025-09-02 16:48:13 -0400

    Add torch 2.7 API list for fuzz testing

--- titanfuzz-tf.sh
+++ titanfuzz-tf.sh
@@ -1,2 +1,2 @@
 # python3 -u run.py --dll tf --version 2.16 --mode cov --itv 60 --time_budget 600 --num_parallel 50 --vs titanfuzz --slurm
-python3 -u run.py --dll tf --version 2.19 --mode fuzz --itv 50 --time_budget 600 --num_parallel 50 --vs titanfuzz --slurm
+python3 -u run.py --dll tf --version 2.19 --mode fuzz --time_budget 600 --num_parallel 50 --vs titanfuzz --slurm
